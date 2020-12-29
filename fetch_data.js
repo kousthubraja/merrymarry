0a06f9f3af000c10159d4daed47069daff5edd89
Kousthub <kousthub.raja@gmail.com>
2020-12-29 16:01:25 +0530

    Filter

--- fetch_data.js
+++ fetch_data.js
@@ -2,6 +2,9 @@ const fetch = require ('node-fetch');
 var fs = require ('fs');
 const https = require ('https');
 
+const outfile = 'kr_profiles.json';
+const MID = 'E5710033';
+
 function handleResponse (res) {
   // let profilesInPage = res.SEARCHRES.PROFILE.filter(p => {
   //   p.PHOTOS.length > 0 
@@ -10,17 +13,17 @@ function handleResponse (res) {
 
   let profilesInPage = res.SEARCHRES.PROFILE;
 
-  var allProfiles = JSON.parse(fs.readFileSync('kr_profiles.json', 'utf8'));
+  var allProfiles = JSON.parse(fs.readFileSync(outfile, 'utf8'));
   allProfiles = allProfiles.concat(profilesInPage);
   var json = JSON.stringify(allProfiles);
-  fs.writeFile('kr_profiles.json', json, 'utf8', ()=>{});
+  fs.writeFile(outfile, json, 'utf8', ()=>{});
 }
 
 var failedPages = [];
 
 async function run () {
   for (var start = 0; start <= 10000; start += 20) {
-    await fetch ('https://mg.bharatmatrimony.com/search/matches/E5710033', {
+    await fetch ('https://mg.bharatmatrimony.com/search/matches/'+MID, {
       headers: {
         accept: 'application/json, text/plain, */*',
         'accept-language': 'en-US,en;q=0.9',
@@ -31,7 +34,7 @@ async function run () {
       },
       referrer: 'https://matches.keralamatrimony.com/',
       referrerPolicy: 'strict-origin-when-cross-origin',
-      body: 'START=' + start + '&APPVERSIONCODE=0&SORT=1&REFINE=0&APPVERSION=6.1&ID=E5652436&LIMIT=20&BLOCKED=1&IGNORED=1&EXCLUDEIDS=null&SHORTLISTED=1&VIEWED=1&CONTACTED=1&ALLRESULTS=8350&PRIMETAG=null&APPTYPE=300&Lang=en&ENCID=c7e3f1d5f6d61564aff22dd13f67b2f32&TOKENID=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDU4ODg1NTcsImp0aSI6IlJUVTJOVEkwTXpZeE5qQTFPRGc0TlRVM0xqa3dNZz09IiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iaGFyYXRtYXRyaW1vbnkuY29tXC8iLCJuYmYiOjE2MDU4ODg1NTcsImV4cCI6MTYwODQ4MDU1NywiZGF0YSI6eyJpZCI6IkU1NjUyNDM2IiwiZW5jbWF0cmlpZCI6InMwJTJGeGVsZk9BcCUyQmcyeldEeGtLWVQlMkJmRTlsWmtUcWpLVnJSU2lCTml6dFElM0QiLCJlbmNwd2QiOiJtMUclMkZOY3BEa0d0R2lPS2NDemRpaWJFdmRFYXdNJTJCRVNlckdOS3VJZnlyayUzRCJ9fQ.2vFRxfkYcFvu44urBCpMlD9GjUIk_Mkt_Uok2v_wcSQ&APIVERSION=1.2&TIMECREATED=2020-10-24 17:18:12&UIVERSION=NEW&WEBPFLAG=1',
+      body: 'START=' + start + '&APPVERSIONCODE=0&SORT=1&REFINE=0&APPVERSION=6.1&ID='+MID+'&LIMIT=20&BLOCKED=1&IGNORED=1&EXCLUDEIDS=null&SHORTLISTED=1&VIEWED=1&CONTACTED=1&ALLRESULTS=0&PRIMETAG=null&APPTYPE=300&Lang=en&ENCID=0fbe846002466f5afe6183e2484a6e5a3&TOKENID=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDkyMzU0MjAsImp0aSI6IlJUVTNNVEF3TXpNeE5qQTVNak0xTkRJd0xqRXlNdz09IiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iaGFyYXRtYXRyaW1vbnkuY29tXC8iLCJuYmYiOjE2MDkyMzU0MjAsImV4cCI6MTYxMTgyNzQyMCwiZGF0YSI6eyJpZCI6IkU1NzEwMDMzIiwiZW5jbWF0cmlpZCI6Ilh2U0wwcG1RUm8lMkZJTjFSYyUyRklDNUxVSEhySExISWVFRXhJeEN2aEtWOVdjJTNEIiwiZW5jcHdkIjoiM0pVOWxrSDdRMlZ2QXZmNyUyRmh2QzJBeHBxQTFSbG9WYkJ2a1lPT1F5aW9JJTNEIn19.ayMMd2gZ9_DEpEzyQeXZM9cTwlSPHn_i5PGk24vsb3M&APIVERSION=1.2&TIMECREATED=2020-11-24 22:06:11&UIVERSION=NEW&WEBPFLAG=1',
       method: 'POST',
       mode: 'cors',
     })
--- index.js
+++ index.js
@@ -17,4 +17,24 @@ app.get('/matches/:user/:start/:stop', function(req, res){
       profiles: profiles
    });
 });
+
+app.get('/matches/:user/filter', function(req, res){
+   let inFile = './avin_profiles.json';
+   if (req.params.user === 'kr'){
+      inFile = './kr_profiles.json';
+   }
+   var profiles = JSON.parse(fs.readFileSync(inFile, 'utf8')).slice(req.params.start, req.params.stop);
+   profiles = profiles.filter(p => p.PHOTOS.length != 0);
+   for (let key in req.query){
+      let value = req.query[key];
+      key = key.toUpperCase();
+      profiles = profiles.filter(p => p[key].includes(value));
+   }
+   
+   res.render('match_view', {
+      start: req.params.start,
+      profiles: profiles
+   });
+});
+
 app.listen(process.env.PORT || 5000);
