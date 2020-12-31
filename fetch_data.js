24d49a4e1fb1e798e9efc048d4d2588aef70657a
Kousthub <kousthub.raja@gmail.com>
2020-12-31 19:14:13 +0530

    Add ashi

--- fetch_data.js
+++ fetch_data.js
@@ -2,8 +2,8 @@ const fetch = require ('node-fetch');
 var fs = require ('fs');
 const https = require ('https');
 
-const outfile = 'kr_profiles.json';
-const MID = 'E5710033';
+const outfile = 'ashi_profiles.json';
+const MID = 'E5454228';
 
 function handleResponse (res) {
   // let profilesInPage = res.SEARCHRES.PROFILE.filter(p => {
@@ -22,7 +22,7 @@ function handleResponse (res) {
 var failedPages = [];
 
 async function run () {
-  for (var start = 0; start <= 10000; start += 20) {
+  for (var start = 1460; start <= 10000; start += 20) {
     await fetch ('https://mg.bharatmatrimony.com/search/matches/'+MID, {
       headers: {
         accept: 'application/json, text/plain, */*',
@@ -34,7 +34,7 @@ async function run () {
       },
       referrer: 'https://matches.keralamatrimony.com/',
       referrerPolicy: 'strict-origin-when-cross-origin',
-      body: 'START=' + start + '&APPVERSIONCODE=0&SORT=1&REFINE=0&APPVERSION=6.1&ID='+MID+'&LIMIT=20&BLOCKED=1&IGNORED=1&EXCLUDEIDS=null&SHORTLISTED=1&VIEWED=1&CONTACTED=1&ALLRESULTS=0&PRIMETAG=null&APPTYPE=300&Lang=en&ENCID=0fbe846002466f5afe6183e2484a6e5a3&TOKENID=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDkyMzU0MjAsImp0aSI6IlJUVTNNVEF3TXpNeE5qQTVNak0xTkRJd0xqRXlNdz09IiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iaGFyYXRtYXRyaW1vbnkuY29tXC8iLCJuYmYiOjE2MDkyMzU0MjAsImV4cCI6MTYxMTgyNzQyMCwiZGF0YSI6eyJpZCI6IkU1NzEwMDMzIiwiZW5jbWF0cmlpZCI6Ilh2U0wwcG1RUm8lMkZJTjFSYyUyRklDNUxVSEhySExISWVFRXhJeEN2aEtWOVdjJTNEIiwiZW5jcHdkIjoiM0pVOWxrSDdRMlZ2QXZmNyUyRmh2QzJBeHBxQTFSbG9WYkJ2a1lPT1F5aW9JJTNEIn19.ayMMd2gZ9_DEpEzyQeXZM9cTwlSPHn_i5PGk24vsb3M&APIVERSION=1.2&TIMECREATED=2020-11-24 22:06:11&UIVERSION=NEW&WEBPFLAG=1',
+      body: 'START=' + start + '&APPVERSIONCODE=0&SORT=1&REFINE=0&APPVERSION=6.1&ID='+MID+'&LIMIT=20&BLOCKED=1&IGNORED=1&EXCLUDEIDS=null&SHORTLISTED=1&VIEWED=1&CONTACTED=1&ALLRESULTS=0&PRIMETAG=null&ANCESTRALSTATE=&APPTYPE=300&Lang=en&ENCID=f538ee0ee8eff17b10c4ce1486dac2007&TOKENID=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDk0MjE5NTcsImp0aSI6IlJUVTBOVFF5TWpneE5qQTVOREl4T1RVM0xqa3pNalk9IiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iaGFyYXRtYXRyaW1vbnkuY29tXC8iLCJuYmYiOjE2MDk0MjE5NTcsImV4cCI6MTYxMjAxMzk1NywiZGF0YSI6eyJpZCI6IkU1NDU0MjI4IiwiZW5jbWF0cmlpZCI6IjVtQVFTQXFwSE90MWtCY1ElMkJSV2ZRZ2RFZzdsNVhpZ1dmVlpaNUhjazYwQSUzRCIsImVuY3B3ZCI6ImVPWmw5Nm9XNWY1bU00bzF1cmhvdGdPeDRCaGJaejhaSmZ4QkwyclVtaDQlM0QifX0.jTrH7I0SSQruTS7R2FssQ9p0iB1Iq7YgfCULvNNwhRc&APIVERSION=1.2&TIMECREATED=2020-06-27 19:27:43&UIVERSION=NEW&WEBPFLAG=1',
       method: 'POST',
       mode: 'cors',
     })
--- index.js
+++ index.js
@@ -6,10 +6,7 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 
 app.get('/matches/:user/:start/:stop', function(req, res){
-   let inFile = './avin_profiles.json';
-   if (req.params.user === 'kr'){
-      inFile = './kr_profiles.json';
-   }
+   let inFile = './' + req.params.user + '_profiles.json';
    var profiles = JSON.parse(fs.readFileSync(inFile, 'utf8')).slice(req.params.start, req.params.stop);
    profiles = profiles.filter(p => p.PHOTOS.length != 0);
    res.render('match_view', {
@@ -19,10 +16,7 @@ app.get('/matches/:user/:start/:stop', function(req, res){
 });
 
 app.get('/matches/:user/filter', function(req, res){
-   let inFile = './avin_profiles.json';
-   if (req.params.user === 'kr'){
-      inFile = './kr_profiles.json';
-   }
+   let inFile = './' + req.params.user + '_profiles.json';
    var profiles = JSON.parse(fs.readFileSync(inFile, 'utf8')).slice(req.params.start, req.params.stop);
    profiles = profiles.filter(p => p.PHOTOS.length != 0);
    for (let key in req.query){
